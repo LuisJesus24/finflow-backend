56d7b65458f8069504545d5fc49a3e67729749f6
Luis Jesus <luis.j.fcfm@gmail.com>
2026-06-29 18:56:26 -0600

    feat: add JWT filter and protect endpoints

--- src/main/java/com/finflow/msvc/users/msvc_users/SecurityConfig.java
+++ src/main/java/com/finflow/msvc/users/msvc_users/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.finflow.msvc.users.msvc_users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
